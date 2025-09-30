0d0c1dbe10fed5162278a3964aabd5e5396b26da
helen-akateva <akateva17@gmail.com>
2025-09-30 11:58:03 +0300

    up

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,5 +1,5 @@
 function getMessage(): Promise<string> {
-  return new Promise<string>((resolve) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve("Hello from TS");
     }, 1000);
